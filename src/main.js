00e98aaa3ddb342e04e446a48119d382a51bb3ba
Khoerodin <khoerodin@live.com>
2017-10-15 18:02:31 +0700

    add router guard

--- src/main.js
+++ src/main.js
@@ -10,13 +10,25 @@ Vue.use(VueRouter)
 Vue.use(VueAxios, axios)
 
 const routes = [
-    {path: '/', component: App, name: 'app'},
-    {path: '/dashboard', component: DashboardPage, name: 'dashboard'}
+    {path: '/', component: App, name: 'home'},
+    {path: '/dashboard', component: DashboardPage, name: 'dashboard', meta: { requiresAuth:true }}
 ]
 
 const router = new VueRouter({
     mode: 'history',
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.requiresAuth) {
+        const authUser = JSON.parse(window.localStorage.getItem('authUser'))
+        if (authUser && authUser.access_token) {
+            next()
+        } else {
+            next({name: 'home'})
+        }
+    }
+    next()
 })
 
 new Vue({
